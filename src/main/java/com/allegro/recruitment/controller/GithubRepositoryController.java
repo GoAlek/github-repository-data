34c3865b101f3e67fd2196cb4ec991c41bc78c2a
GoAlek <alek.golaszewski@gmail.com>
2017-10-28 17:12:31 +0200

    added path variables

--- src/main/java/com/allegro/recruitment/controller/GithubRepositoryController.java
+++ src/main/java/com/allegro/recruitment/controller/GithubRepositoryController.java
@@ -1,12 +1,17 @@
 package com.allegro.recruitment.controller;
 
 import com.allegro.recruitment.controller.dto.RepositoryDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/repositories")
 public class GithubRepositoryController {
 
-    public RepositoryDetails getRepositoryDetails() {
+    @GetMapping("/{owner}/{repository-name}")
+    public RepositoryDetails getRepositoryDetails(
+            @PathVariable String owner,
+            @PathVariable("repository-name") String repositoryName) {
         return null;
     }
 }
