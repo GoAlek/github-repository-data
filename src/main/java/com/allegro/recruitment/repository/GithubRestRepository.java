f74a65cfccd24f2b74539343d94adfd5870fa47e
GoAlek <alek.golaszewski@gmail.com>
2017-10-30 23:30:52 +0100

    externalized application configuration

--- src/main/java/com/allegro/recruitment/repository/GithubRestRepository.java
+++ src/main/java/com/allegro/recruitment/repository/GithubRestRepository.java
@@ -1,25 +1,26 @@
 package com.allegro.recruitment.repository;
 
 import com.allegro.recruitment.repository.dto.GithubRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
-
-import java.text.MessageFormat;
 
 @Repository
 public class GithubRestRepository {
 
-    private final String gitHubApiUrl = "https://api.github.com/repos/{0}/{1}";
+    private final RestTemplate restTemplate;
 
-    private RestTemplate restTemplate;
-
-    public GithubRestRepository() {
-        this.restTemplate = new RestTemplate();
+    @Autowired
+    public GithubRestRepository(
+            RestTemplateBuilder restTemplateBuilder,
+            @Value("${github-root-uri}") String gitHubRootUri) {
+        this.restTemplate = restTemplateBuilder.rootUri(gitHubRootUri).build();
     }
 
     public GithubRepository getGithubRepository(String owner, String repositoryName) {
-        final String requestUrl = MessageFormat.format(gitHubApiUrl, owner, repositoryName);
-        return restTemplate.getForObject(requestUrl, GithubRepository.class);
+        return restTemplate.getForObject("/repos/{owner}/{repo}", GithubRepository.class, owner, repositoryName);
     }
 
 
