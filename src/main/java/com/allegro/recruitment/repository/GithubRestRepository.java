b673a4d57159d7c08d192b0355e2f1a5ef1af573
GoAlek <alek.golaszewski@gmail.com>
2017-10-31 14:03:37 +0100

    specified qualifier for GithubRestRepository

--- src/main/java/com/allegro/recruitment/repository/GithubRestRepository.java
+++ src/main/java/com/allegro/recruitment/repository/GithubRestRepository.java
@@ -1,10 +1,10 @@
 package com.allegro.recruitment.repository;
 
-import com.allegro.recruitment.interceptors.GithubHeaderRequestInterceptor;
 import com.allegro.recruitment.repository.dto.GithubRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,19 +13,18 @@ public class GithubRestRepository {
 
     private final RestTemplate restTemplate;
 
-    @Autowired
     public GithubRestRepository(
-            @Value("${github-root-uri}") String gitHubRootUri,
             RestTemplateBuilder restTemplateBuilder,
-            GithubHeaderRequestInterceptor githubHeaderRequestInterceptor) {
+            @Value("${github-root-uri}") String gitHubRootUri,
+            @Qualifier("githubHeaderRequestInterceptor") ClientHttpRequestInterceptor clientHttpRequestInterceptor) {
         this.restTemplate = restTemplateBuilder
                 .rootUri(gitHubRootUri)
-                .interceptors(githubHeaderRequestInterceptor)
+                .interceptors(clientHttpRequestInterceptor)
                 .build();
     }
 
     public GithubRepository getGithubRepository(String owner, String repositoryName) {
-        return restTemplate.getForObject("/repos/{owner}/{repo}", GithubRepository.class, owner, repositoryName);
+        return restTemplate.getForObject("/repos/{owner}/{repo-name}", GithubRepository.class, owner, repositoryName);
     }
 
 
