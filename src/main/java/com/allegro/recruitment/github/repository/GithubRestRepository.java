9ec5c896341080aa4aaa367b4bba062983def44b
GoAlek <alek.golaszewski@gmail.com>
2017-12-06 13:22:21 +0100

    code review, added configuration for github repository uri

--- src/main/java/com/allegro/recruitment/github/repository/GithubRestRepository.java
+++ src/main/java/com/allegro/recruitment/github/repository/GithubRestRepository.java
@@ -2,20 +2,25 @@ package com.allegro.recruitment.github.repository;
 
 import com.allegro.recruitment.github.repository.dto.GithubRepoData;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
 @Repository
 public class GithubRestRepository {
 
+    private final String gitHubReposUri;
     private final RestTemplate githubRestTemplate;
 
-    public GithubRestRepository(@Qualifier("githubRestTemplate") RestTemplate githubRestTemplate) {
+    public GithubRestRepository(
+            @Value("${github-repos-uri}") String gitHubReposUri,
+            @Qualifier("githubRestTemplate") RestTemplate githubRestTemplate) {
+        this.gitHubReposUri = gitHubReposUri;
         this.githubRestTemplate = githubRestTemplate;
     }
 
     public GithubRepoData getGithubRepositoryData(String owner, String repositoryName) {
-        return githubRestTemplate.getForObject("/repos/{owner}/{repo-name}", GithubRepoData.class, owner, repositoryName);
+        return githubRestTemplate.getForObject(gitHubReposUri, GithubRepoData.class, owner, repositoryName);
     }
 
     public RestTemplate getGithubRestTemplate() {
